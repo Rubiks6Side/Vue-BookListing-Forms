3c502ee0daaecd75e13c885e0f58ed4d626b3868
AJ Helfen <ahelfen@SCLTMAC-10081.local>
2019-01-14 13:12:47 -0500

    Check module 4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input v-model="searchInput" type="text" name="searchInput" placeholder="Search Books">
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
     <h2>Filtered Books By Ownership</h2>
@@ -28,8 +29,6 @@ export default {
     return {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
-      filters: ["bought", "borrowed"],
-      holding: "bought",
       books: [
         { 
           title: "Self-Reliance", 
@@ -49,7 +48,10 @@ export default {
           finishedReading: true,
           ownership: "borrowed", 
         }
-      ]
+      ],
+      filters: ["bought", "borrowed"],
+      holding: "bought",
+      searchInput: ""
     };
   },
   components: {
@@ -59,6 +61,13 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      }
+
+      return  _.filter(this.books, searchFilter);
     },
   },
   methods: {
